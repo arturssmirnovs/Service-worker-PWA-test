d25cb047ed514cb125a8cd6ba570d898e719fc7e
Arturs Smirnovs <yam.aka.as@gmail.com>
2020-07-12 10:57:25 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,14 +2,14 @@ self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open('myapp-static-v1').then(function(cache) {
             return cache.addAll([
-                'https://github.com/arturssmirnovs/Service-worker-PWA-test/Service-worker-PWA-test/favicon.png',
-                'https://github.com/arturssmirnovs/Service-worker-PWA-test/Service-worker-PWA-test/no.mp3',
-                'https://github.com/arturssmirnovs/Service-worker-PWA-test/Service-worker-PWA-test/yes.mp3',
-                'https://github.com/arturssmirnovs/Service-worker-PWA-test/Service-worker-PWA-test/on.svg',
-                'https://github.com/arturssmirnovs/Service-worker-PWA-test/Service-worker-PWA-test/off.svg',
-                'https://github.com/arturssmirnovs/Service-worker-PWA-test/Service-worker-PWA-test/style.css',
-                'https://github.com/arturssmirnovs/Service-worker-PWA-test/Service-worker-PWA-test/script.js',
-                'https://github.com/arturssmirnovs/Service-worker-PWA-test/Service-worker-PWA-test/index.html'
+                'favicon.png',
+                'no.mp3',
+                'yes.mp3',
+                'on.svg',
+                'off.svg',
+                'style.css',
+                'script.js',
+                'index.html'
             ]);
         })
     )
