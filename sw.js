84873ccbb94f58eababf3e7d0341c6092b3200fb
Arturs Smirnovs <yam.aka.as@gmail.com>
2020-07-12 10:53:27 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,14 +2,14 @@ self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open('myapp-static-v1').then(function(cache) {
             return cache.addAll([
-                '/Service-worker-PWA-test/favicon.png',
-                '/Service-worker-PWA-test/no.mp3',
-                '/Service-worker-PWA-test/yes.mp3',
-                '/Service-worker-PWA-test/on.svg',
-                '/Service-worker-PWA-test/off.svg',
-                '/Service-worker-PWA-test/style.css',
-                '/Service-worker-PWA-test/script.js',
-                '/Service-worker-PWA-test/index.html'
+                './Service-worker-PWA-test/favicon.png',
+                './Service-worker-PWA-test/no.mp3',
+                './Service-worker-PWA-test/yes.mp3',
+                './Service-worker-PWA-test/on.svg',
+                './Service-worker-PWA-test/off.svg',
+                './Service-worker-PWA-test/style.css',
+                './Service-worker-PWA-test/script.js',
+                './Service-worker-PWA-test/index.html'
             ]);
         })
     )
